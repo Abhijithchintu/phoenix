f10935f343364445f8685cd96e5d0b06a811b384
Abhijith Goolla <goolla.abhijith@paytm.com>
2023-01-07 18:06:49 +0530

    initial test

--- Oauth/routes/index.js
+++ Oauth/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+router.use(express.static(path.resolve(__dirname, '../../phoenixfe/build')));
+router.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../../phoenixfe/build', 'index.html'));
 });
-
 module.exports = router;
