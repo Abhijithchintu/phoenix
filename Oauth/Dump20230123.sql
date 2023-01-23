5aff22d7df4a8275249eed0db3c9bd377338ae88
manu <manu156@users.noreply.github.com>
2023-01-23 17:50:21 +0530

    Add files via upload

---
+++ Oauth/Dump20230123.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE  IF NOT EXISTS `phoenixOauth` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `phoenixOauth`;
+
+CREATE TABLE `users` (
+  `client_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `status` tinyint(2) DEFAULT NULL,
+  `user_name` varchar(32) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `gender` tinyint(1) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `photo_id` bigint(20) DEFAULT NULL,
+  `password` char(30) NOT NULL,
+  `mobile` varchar(15) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`client_id`),
+  KEY `idx_updated_at` (`updated_at`),
+  KEY `mobile` (`mobile`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
