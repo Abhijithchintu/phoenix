3b6a8ca7b239934592446f138e1220d717e88e93
Rakesh <anugur@iitbhilai.ac.in>
2023-09-02 14:49:24 +0530

    Socket

--- Chat/app.js
+++ Chat/app.js
@@ -23,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-// app.use('/server-client', serverRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
